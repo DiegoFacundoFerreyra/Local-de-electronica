232bee51d685d2f2d6c930fa83319dcef11bc751
Diego Ferreyra <diegofacundoferreyra@gmail.com>
2025-08-13 20:43:41 -0300

    Arrancando con JS

---
+++ main.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Nombre");
+/* let apellido = prompt("Apellido");
+let fechaDeNacimiento = prompt("Fecha de nacimiento");
+let dNI = prompt("DNI");
+let dirección = prompt("Dirección");
+let ciudad = prompt("Ciudad");
+let teléfono = prompt("Teléfono");
+let correoeElectrónico = prompt("Correo electrónico");
+let usuario = prompt("Usuario");
+let contraseña = prompt("Contraseña"); */
+
+alert("Bienvenido " + nombre);
