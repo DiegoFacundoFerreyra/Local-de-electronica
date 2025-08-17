8aa6da06ff73f743e2e85a796a98ddd6c289aff7
Diego Ferreyra <diegofacundoferreyra@gmail.com>
2025-08-17 15:07:00 -0300

    Arrancando anotaciones de JS

--- main.js
+++ main.js
@@ -10,7 +10,49 @@ let usuario = prompt("Usuario");
 let contraseña = prompt("Contraseña"); */
 /* alert("Bienvenido " + nombre); */
 
+/* 12 ES UN NUMERO Y "12" ES UN TEXTO/* 
+
 /* numero1 = 10;
 numero2 = 8;
 resultado = numero1 + numero2;
 alert(resultado); */
+
+/* OPERADORES DE COMPARACION
+(verdadero o falso)
+= asigna
+== compara pero no diferencia si es un texto o un numero
+=== solo identifica texto con texto y numero con numero
+!= valor Y es dinstinto a valor X?
+!== valor Y es estricamente igual a valor X? 
+numero1 <= nuermo2 (menor o igual)
+numero1 >= numero2 (mayor o igual)
+numero1 < numero2 (menor que)
+numero2 > numero2 (mayor que)*/
+
+/* let numero1 = 24;
+let numero2 = 14;
+alert(numero1 == numero2); FALSE*/
+
+/* let numero1 = 8;
+let numero2 = 22;
+alert(numero1 != numero2) TRUE; */
+
+/* let numero1 = 22;
+let numero2 = 33;
+let resultado = numero1 == numero2;
+alert(resultado); */
+
+/*OPERADORES LOGICOS
+&& si 1 sola de 2 es false devuelve false 
+|| si 1 de las 2 es verdadera, devuelve verdadera
+! me devuelve lo contrario de lo que hay a continuacion (! va en el parentecis del resultado)*/
+
+/*CONDICIONALES
+IF = si esta condicion se cumple (ESTE ES EL PRIMERO Y ES 1 SOLO)
+ELSE IF = si no, vamos a ver si esta otra condicion se cumple (ESTE VA ENTRE EL "IF" Y EL "ELSE" Y PODEMOS PONER TODOS LOS QUE QUERRAMOS)
+ELSE = si no se ejecuto ninguno de los anteriores, muestra este. (ESTE ES EL ULTIMO Y ESE 1 SOLO)
+{todo lo que esta aca adentro es un bloque.
+y los bloques se ejecutan}
+if (10 > 5) {ejecuta lo de aca adentro solo si es TRUE, si es FALSE no ejecuto, en este caso,
+10 es mayor que 5 asique lo ejecutaria, en caso que fuese menor, seria false y ahi no ejecutaria.}
+else if (MUESTRA SI LA SOLUCION NO SE CUMPLE)*/
